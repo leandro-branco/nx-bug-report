2dda2cbdf846b30b090a518a16b1c9062ab3085c
Leandro Branco <leandro@leandrobranco.com>
2023-06-06 19:25:42 -0300

    initial commit

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'nx-fail-root',
+  imports: [RouterModule],
+  selector: 'nxf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
